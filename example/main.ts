9ca95111aaac2910a2cdf3e7d36598aa5b65ca27
danigb <danigb@gmail.com>
2023-03-11 09:29:13 +0100

    feat: example

--- example/main.ts
+++ example/main.ts
@@ -1,17 +1,70 @@
-import { Synthlet } from "../src";
+import { Mika, MikaPresets } from "../src";
+
+const DEBUG = true;
 
 main();
 
 async function main() {
   const context = new AudioContext();
-  console.log("Synthlet");
-  const synthlet = await new Synthlet(context).loaded();
+  context.resume();
+  console.log("Mika");
+  const synth = await new Mika(context).loaded();
 
-  window.document.onclick = () => {
-    console.log("Trigger");
-    synthlet.setNote(60, context.currentTime);
-    synthlet.start(context.currentTime);
-    synthlet.setNote(64, context.currentTime + 1);
-    synthlet.release(context.currentTime + 2);
-  };
+  if (DEBUG && synth.worklet) {
+    console.log(
+      "WORKLET",
+      synth.paramNames,
+      synth.worklet.parameters,
+      Array.from(synth.worklet.parameters.keys())
+    );
+    synth.worklet.port.onmessage = (e) => {
+      console.log("MSG >>>", e.data);
+    };
+  }
+
+  const presetsNames = MikaPresets.map((k) => k.name).sort();
+
+  const presetDisplay = document.getElementById("preset") as HTMLPreElement;
+  const select = document.getElementById("preset-select") as HTMLSelectElement;
+  select.innerHTML = presetsNames
+    .map((name) => `<option value="${name}">${name}</option>`)
+    .join("\n");
+
+  function selectPreset(presetName: string) {
+    const preset = MikaPresets.find((p) => p.name === presetName);
+    if (!preset) return;
+
+    synth.setPreset(preset);
+    presetDisplay.innerText = JSON.stringify(preset, null, 2);
+  }
+
+  const randomPresetName =
+    presetsNames[Math.floor(Math.random() * presetsNames.length)];
+  selectPreset(randomPresetName);
+  select.value = randomPresetName;
+
+  select.addEventListener("change", (e) => {
+    selectPreset((e?.target as any)?.value as string);
+  });
+
+  const noteButtons = document.getElementsByClassName("btn-note");
+  for (const btn of noteButtons) {
+    const note = parseInt(btn.getAttribute("data-note") ?? "60", 10);
+    btn.addEventListener("mousedown", () => {
+      synth.setNote(note);
+      synth.start();
+    });
+    btn.addEventListener("mouseup", () => {
+      synth.release();
+    });
+  }
+
+  function resumeContext() {
+    context.resume();
+    document.body.classList.remove("opacity-20");
+    document.body.removeEventListener("click", resumeContext);
+  }
+
+  document.body.classList.add("opacity-20");
+  document.body.addEventListener("click", resumeContext);
 }
