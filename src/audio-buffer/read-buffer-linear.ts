743c2c89642417af4258e8542b4ab0e39e69b1da
danigb <danigb@gmail.com>
2023-10-08 23:30:59 +0200

    feat: linear audio buffer reader

---
+++ src/audio-buffer/read-buffer-linear.ts
@@ -0,0 +1,33 @@
+export type ReadAudioBufferLinear = {
+  read(inc: number): number;
+  set(index: number, bottom?: number, top?: number): void;
+};
+
+export function readBufferLinear(buffer: Float32Array): ReadAudioBufferLinear {
+  const $buffer = buffer;
+  let $len = buffer.length;
+  let $index = 0;
+  let $bottom = 0;
+
+  function read(inc: number) {
+    const index = Math.floor($index);
+    const frac = $index - index;
+    const next = inc > 0 ? index + 1 : index - 1;
+    const nextIndex = next < 0 ? $len - 1 : next >= $len ? 0 : next;
+    const y1 = $buffer[$bottom + index];
+    const y2 = $buffer[$bottom + nextIndex];
+    const y = y1 + (y2 - y1) * frac;
+    $index += inc;
+    if ($index >= $len) $index -= $len;
+    if ($index < 0) $index += $len;
+    return y;
+  }
+
+  function set(index: number, bottom?: number, len?: number) {
+    $bottom = Math.floor(bottom ?? 0);
+    $len = Math.floor(Math.min(len ?? $len, $buffer.length));
+    $index = index % $len;
+  }
+
+  return { read, set };
+}
