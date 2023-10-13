30bf20813c63a1bfa2020fe9ccd52c01d3596d70
danigb <danigb@gmail.com>
2023-10-13 18:37:30 +0200

    chore: remove rhythm

--- src/rhythm/rhythm.ts
+++
@@ -1,9 +0,0 @@
-import { ParamsDef } from "../params-utils";
-
-export const PARAM_DEFS: ParamsDef = {
-  bpm: { min: 0, max: 300, init: 120 },
-};
-
-export function Rhythm() {
-  function fillControl() {}
-}
