ef27421a9e02e97c6bb1be68ab97916c05ae488d
danigb <danigb@gmail.com>
2024-07-27 15:10:47 +0200

    feat: create site

---
+++ site/README.md
@@ -0,0 +1,1 @@
+# Synthlet site
---
+++ site/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-24">
+      <h1 className="text-2xl">Synthlet</h1>
+      <p className="text-gray-400">
+        A collection of synth modules implemented as AudioWorklets
+      </p>
+    </main>
+  );
+}
