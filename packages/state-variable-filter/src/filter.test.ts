339937e6821787b5fa26e20b3073ee506ab05511
danigb <danigb@gmail.com>
2024-07-27 18:14:15 +0200

    feat: improve filter api

--- packages/state-variable-filter/src/filter.test.ts
+++ packages/state-variable-filter/src/filter.test.ts
@@ -1,23 +1,28 @@
-import { SVFilter, SVFilterType } from "./filter";
+import { SVFilter } from "./filter";
 
 describe("SVFilter", () => {
   it("filters the signal", () => {
-    const filter = SVFilter(20, SVFilterType.LowPass);
+    const filter = SVFilter(20);
     const input = new Float32Array(20);
+    const output = new Float32Array(20);
+
     for (let i = 0; i < input.length; i++) {
       input[i] = i % 2 ? 1 : -1;
     }
     expect(input).toMatchSnapshot();
-    const output = new Float32Array(20);
-    filter.fill(input, output, {
+    filter.update({
+      filterType: [1],
       frequency: [10],
       resonance: [0.5],
     });
+    filter.fill(input, output);
     expect(output).toMatchSnapshot();
-    filter.fill(input, output, {
+    filter.update({
+      filterType: [1],
       frequency: [5],
       resonance: [0.5],
     });
+    filter.fill(input, output);
     expect(output).toMatchSnapshot();
   });
 });
--- packages/state-variable-filter/src/index.ts
+++ packages/state-variable-filter/src/index.ts
@@ -1,26 +1,31 @@
 import { PROCESSOR } from "./processor";
 
-export type ProcessorOptions = {
-  mode?: "generator" | "modulator";
+export type StateVariableFilterOptions = {
+  type: "bypass" | "lowpass" | "bandpass" | "hipass";
+  filterType: number;
+  frequency: number;
+  resonance: number;
 };
-
 export type StateVariableFilterParams = {
+  filterType: number;
   frequency: number;
   resonance: number;
 };
 
 export type StateVariableFilterWorkletNode = AudioWorkletNode & {
+  type: string;
+  filterType: AudioParam;
   frequency: AudioParam;
   resonance: AudioParam;
 };
 
-const PARAM_NAMES = ["frequency", "resonance"] as const;
+const PARAM_NAMES = ["filterType", "frequency", "resonance"] as const;
 
-export function getProcessorName() {
+export function getStateVariableFilterProcessorName() {
   return "StateVariableFilterWorkletProcessor"; // Can't import from worklet because globals
 }
 
-export function getWorkletUrl() {
+function getWorkletUrl() {
   const blob = new Blob([PROCESSOR], { type: "application/javascript" });
   return URL.createObjectURL(blob);
 }
@@ -64,28 +69,36 @@ export function registerStateVariableFilterWorkletOnce(
  * @returns StateVariableFilterAudioWorkletNode
  */
 export function createStateVariableFilter(
-  audioContext: AudioContext
-): StateVariableFilterWorkletNode {
-  return createWorkletNode(audioContext, {});
-}
-
-function createWorkletNode(
   audioContext: AudioContext,
-  processorOptions: ProcessorOptions,
-  params: Partial<StateVariableFilterParams> = {}
+  options: Partial<StateVariableFilterOptions> = {}
 ): StateVariableFilterWorkletNode {
-  const node = new AudioWorkletNode(audioContext, getProcessorName(), {
-    numberOfInputs: 1,
-    numberOfOutputs: 1,
-    processorOptions,
-  }) as StateVariableFilterWorkletNode;
+  const params = optionsToParams(options);
+  const node = new AudioWorkletNode(
+    audioContext,
+    getStateVariableFilterProcessorName(),
+    {
+      numberOfInputs: 1,
+      numberOfOutputs: 1,
+    }
+  ) as StateVariableFilterWorkletNode;
 
   for (const paramName of PARAM_NAMES) {
     const param = node.parameters.get(paramName)!;
     node[paramName] = param;
     const value = params[paramName as keyof StateVariableFilterParams];
-    if (typeof value === "number") param.setValueAtTime(value, 0);
+    param.setValueAtTime(value, 0);
   }
+
+  Object.defineProperty(node, "type", {
+    get() {
+      return filterTypeToType(this.filterType.value);
+    },
+    set(value: string) {
+      const filterType = typeToFilterType(value);
+      this.filterType.setValueAtTime(filterType, 0);
+    },
+  });
+
   let _disconnect = node.disconnect.bind(node);
   node.disconnect = (param?, output?, input?) => {
     node.port.postMessage({ type: "DISCONNECT" });
@@ -93,4 +106,45 @@ function createWorkletNode(
     return _disconnect(param, output, input);
   };
   return node;
+}
+
+function optionsToParams(
+  options: Partial<StateVariableFilterOptions>
+): StateVariableFilterParams {
+  if (options.type !== undefined && options.filterType !== undefined) {
+    throw Error(
+      "PolyblepOscillator error - only filterType or type can be defined in options"
+    );
+  }
+
+  const frequency =
+    typeof options.frequency === "number" ? options.frequency : 4000;
+  const resonance =
+    typeof options.resonance === "number" ? options.resonance : 0.5;
+  const filterType =
+    typeof options.filterType === "number"
+      ? options.filterType
+      : typeToFilterType(options.type) || 1;
+
+  return { frequency, resonance, filterType };
+}
+
+function filterTypeToType(filterType: number) {
+  return filterType === 1
+    ? "lowpass"
+    : filterType === 2
+    ? "bandpass"
+    : filterType === 3
+    ? "highpass"
+    : "bypass";
+}
+
+function typeToFilterType(type?: string): number {
+  return type === "lowpass"
+    ? 1
+    : type === "bandpass"
+    ? 2
+    : type === "highpass"
+    ? 3
+    : 0;
 }
