990ce536118588b6f21a460e47d5ce36c8a5c9b2
danigb <danigb@gmail.com>
2024-09-09 12:19:40 +0200

    fix: tests

--- packages/state-variable-filter/src/filter.test.ts
+++ packages/state-variable-filter/src/filter.test.ts
@@ -2,24 +2,23 @@ import { createFilter, SvfType } from "./dsp";
 
 describe("SVFilter", () => {
   it("filters the signal", () => {
-    const filter = createFilter(20)(SvfType.LowPass);
+    const filter = createFilter(20);
     const input = new Float32Array(20);
     const output = new Float32Array(20);
     const frequency = new Float32Array(20);
-    const q = new Float32Array(20);
+    const type = SvfType.LowPass;
+    const q = 0.5;
 
     for (let i = 0; i < input.length; i++) {
       input[i] = i % 2 ? 1 : -1;
     }
     expect(input).toMatchSnapshot();
     frequency.fill(10);
-    q.fill(0.5);
-    filter(input, output, frequency, q);
+    filter(input, output, type, frequency, q);
     expect(output).toMatchSnapshot();
 
     frequency.fill(5);
-    q.fill(0.5);
-    filter(input, output, frequency, q);
+    filter(input, output, type, frequency, q);
     expect(output).toMatchSnapshot();
   });
 });
